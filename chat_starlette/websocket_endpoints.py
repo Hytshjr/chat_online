b55ecdcb979177aac2a2adabc76a3c25123379f6
hytsh <celixscondolo@gmail.com>
2024-06-21 22:35:41 -0500

    refactor consumer

--- chat_starlette/websocket_endpoints.py
+++ chat_starlette/websocket_endpoints.py
@@ -1,29 +1,52 @@
 from starlette.websockets import WebSocket
 import json
 
-# In-memory storage for groups (for simplicity, not suitable for production)
+
+class ConnectionManager:
+    _instance = None
+
+
+    def __new__(cls, *args, **kwargs):
+        if not cls._instance:
+            cls._instance = super(ConnectionManager, cls).__new__(cls, *args, **kwargs)
+        return cls._instance
+
+
+    def __init__(self):
+        self.connections = {}
+
+
+    def add_connection(self, room_name, connection):
+        if room_name not in self.connections:
+            self.connections[room_name] = []
+        self.connections[room_name].append(connection)
+
+
+    def remove_connection(self, room_name, connection):
+            if room_name in self.connections:
+                self.connections[room_name].remove(connection)
+                if not self.connections[room_name]:
+                    del self.connections[room_name]
+
+
+connection_manager = ConnectionManager()
+
 groups = {}
 
 async def websocket_endpoint(websocket: WebSocket):
     await websocket.accept()
-    room_name = websocket.query_params.get('room_name')
-    room_group_name = f'chat_{room_name}'
-
-    if room_group_name not in groups:
-        groups[room_group_name] = []
-
-    groups[room_group_name].append(websocket)
+    room_name = websocket.path_params['room_name']
+    connection_manager.add_connection(room_name, websocket)
 
     try:
         while True:
             data = await websocket.receive_text()
-            message = json.loads(data)  # Parse the received message
-            # Send message to all in the group
-            for connection in groups[room_group_name]:
+            message = json.loads(data)
+            for connection in connection_manager.connections[room_name]:
                 await connection.send_text(json.dumps(message))
     except Exception as e:
         print(f"Error: {e}")
     finally:
-        groups[room_group_name].remove(websocket)
-        if not groups[room_group_name]:
-            del groups[room_group_name]
+        connection_manager.remove_connection(room_name, websocket)
+
+
